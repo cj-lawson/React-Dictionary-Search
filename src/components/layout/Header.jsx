827cb93bdba47ce7f4b5a995402e98e986abaf50
CJ Lawson <clawson@mt06kw6pkw3.mynetworksettings.com>
2024-02-02 14:56:29 -0600

    github logo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <nav className="w-full">
       <div className="flex justify-between w-full">
-        <img src={logo} alt="book outline" className="w-5" />
+        <img src={logo} alt="book outline" className="w-7" />
         <div className="flex space-x-2">
           <a href="https://github.com/cj-lawson/React-Dictionary-Search">
-            <img src={github} alt="book outline" className="w-5" />
+            <img src={github} alt="book outline" className="w-7" />
           </a>
         </div>
       </div>
