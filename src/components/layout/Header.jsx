e2b29d70da75a20f04b8bad7627e6cef27647215
CJ Lawson <clawson@mt06kw6pkw3.mynetworksettings.com>
2024-02-02 14:51:14 -0600

    added github anchor

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "/logo.svg";
-import chevronDown from "/chevron-down.svg";
+import github from "/github-mark-white.svg";
 
 const Header = () => {
   return (
@@ -7,8 +7,9 @@ const Header = () => {
       <div className="flex justify-between w-full">
         <img src={logo} alt="book outline" className="w-5" />
         <div className="flex space-x-2">
-          <h1>Sans Serif</h1>
-          <img src={chevronDown} alt="book outline" className="w-3" />
+          <a href="https://github.com/cj-lawson/React-Dictionary-Search">
+            <img src={github} alt="book outline" className="w-5" />
+          </a>
         </div>
       </div>
     </nav>
